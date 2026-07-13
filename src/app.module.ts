10c5557f9747113d633f69e9afd2d403a657105e
thanhnguyen1502 <thanhlucky150204@gmail.com>
2026-07-13 09:27:32 +0700

    init project and setup environment

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { WorkspaceModule } from './workspaces/workspace.module';
+import { MembershipModule } from './memberships/membership.module';
+import { ProjectsModule } from './projects/projects.module';
+import { TasksModule } from './tasks/tasks.module';
+import { CommentsModule } from './comments/comments.module';
+import { AttachmentsModule } from './attachments/attachments.module';
+import { ActivityLogsModule } from './activity/activity-logs.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { CommonModule } from './common/common.module';
+
+@Module({
+  imports: [AuthModule, UsersModule, WorkspaceModule, MembershipModule, ProjectsModule, TasksModule, CommentsModule, AttachmentsModule, ActivityLogsModule, PrismaModule, CommonModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
